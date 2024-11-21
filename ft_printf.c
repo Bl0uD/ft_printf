96e2c3ac1468237fbd49dc49c3b915a22a493d44
root <root@DESKTOP-5TA7O8P>
2024-11-21 23:41:08 +0100

    auto update

--- ft_printf.c
+++ ft_printf.c
@@ -3,19 +3,113 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ju <ju@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 20:37:26 by ju                #+#    #+#             */
-/*   Updated: 2024/11/21 20:36:27 by ju               ###   ########.fr       */
+/*   Updated: 2024/11/21 23:21:06 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
+#include "../LibFT/libft.h"
 #include <stdarg.h>
 #include <unistd.h>
 
-int	ft_printf(const char*, ...)
+size_t	ft_vdputchar(int fd, va_list list)
 {
-	va_list	coucou;
-	
+	char	c;
+
+	c = va_arg(list, int);
+	write(fd, &c, 1);
+
+	return (1);
+}
+
+size_t	ft_vdputstr(int fd, va_list list)
+{
+	char	*str;
+
+	str = va_arg(list, char *);
+	ft_putstr_fd(str, fd);
+	return (ft_strlen(str));
+}
+
+size_t	ft_vdputnbr(int fd, va_list list)
+{
+	int	d;
+
+	d = va_arg(list, int);
+	ft_putnbr_fd(d, fd);
+	return (3);
+}
+
+struct s_printf_flag
+{
+	char	flag;
+	size_t	(*f)(int fd, va_list list);
+};
+
+static struct s_printf_flag flags[] = {
+	{
+		.flag = 'c',
+		.f = ft_vdputchar
+	},
+	{
+		.flag = 's',
+		.f = ft_vdputstr
+	},
+	{
+		.flag = 'd',
+		.f = ft_vdputnbr
+	}
+};
+
+int	ft_vdprintf(int fd, const char *format, va_list ap)
+{
+	int	i;
+	int	j;
+	int	res;
+
+	i = 0;
+	res = 0;
+	while (format[i])
+	{
+		j = 0;
+		while (j < (sizeof(flags) / sizeof(*flags)))
+		{
+			if (format[i] == flags[j].flag)
+			{
+				res = res + flags[j].f(fd, ap);
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (res);
+}
+
+int	ft_dprintf(int fd, const char *format, ...)
+{
+	va_list	ap;
+	int		i;
+
+	va_start(ap, format);
+	ft_vdprintf(fd, format, ap);
+	va_end(ap);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+
+	va_start(ap, format);
+	ft_vdprintf(1, format, ap);
+	va_end(ap);
+}
+
+int	main(void)
+{
+	ft_printf("dsc", 12, "Hello world", 'c');
+
+	return (0);
 }
